9acc6ed5a37e4f8ba37dc6f75e8cb3a43161b8c3
jamesmwebelevate <james.morrissey.github@gmail.com>
2017-10-31 17:20:08 +0000

    Removing old code

--- app.js
+++ app.js
@@ -7,43 +7,35 @@ const pug = require('pug');
 var app = express();
 
 //  bodyParser middleware
-
-//Parses json data
+//  Parses json data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-var person = [{
-  name : 'Jeff',
-  age : 4
-},
-{
-  name : 'Jeff',
-  age : 4
-}
-,
-{
-  name : 'Jeff',
-  age : 4
-}];
+
 
 
 //  Sets a static paths where all the files are stored.
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('assets'));
 
+
+
+
 //  Configuring the pug middleware
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+
+
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
-//  home page
-//app.get('/',function (request, response) {
-//  response.send(person);
-//});
+
+
+
 
 app.listen(3000, function () {
-  console.log("Server running on port 3000...");
+  console.log("Server running on port 3000...\n Press Ctrl & C to end the server.");
 });
