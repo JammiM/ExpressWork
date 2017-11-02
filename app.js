8ef9677463be4e290f8e07a5f326c6de596c16a1
JammiM <james.morrissey.github@gmail.com>
2017-11-02 10:46:02 +0000

    Added a error and sucess page to display information back to the user

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const pug = require('pug');
 const expressValidator = require('express-validator');
+const mongojs = require('mongojs')
 
 var app = express();
-var cutomers = [{fn:'james'},{fn:'jack'},{fn:'jamie'}];
+var users = [{fn:'james',em:'James@gmail.com'},{fn:'jack',em:'Jack@gmail.com'},{fn:'jamie',em:'Jamie@gmail.com'}];
+//var db = mongojs('customerapp', ['users']);
 
 //  BodyParser middleware (parses json data)
 app.use(bodyParser.json());
@@ -18,8 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false}));
 //  Sets a static paths where all the files are stored.
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static('assets'));
-//app.use(express.static(__dirname + '/public'));
-//app.use('/public', express.static(path.join(__dirname , '/public')));
+
 
 
 //  Express Validator Middleware
@@ -35,27 +36,44 @@ app.set('view engine', 'pug');
 
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!', users: cutomers })
+
+  // find everything
+/*  db.users.find(function (err, docs) {
+      // docs is an array of all the documents in mycollection
+      console.log(docs);
+      res.render('index', { title: 'People in database',
+                            message: 'People in database',
+                            users: docs });
+  });  */
+
+  res.render('index', { title: 'People in database',
+                        message: 'People in database',
+                        users: users});
 });
 
 
 // Handler for the form post request
 app.post('/users/add', function (req, res) {
 
+  //Checks if the input field is not Empty
   req.checkBody('firstName', 'First name is required').notEmpty();
+  req.checkBody('email', 'Enter a valid email address.').isEmail();
 
+
+  // sets errors to true, if there is a errors
   var errors = req.validationErrors();
 
   if(errors){
-      console.log('there was a error');
-      res.render('index', { title: 'Hey', message: 'Hello there!', users: cutomers, errors: errors });
+      console.log('There was a error');
+      // Print the error on the error page.
+      res.render('error', { title: 'Errors', message: 'There was errors !', users: users, errors: errors });
   }else {
-    var newUser = { fn: req.body.firstName };
+      var newUser = { fn: req.body.firstName };
+      console.log('Success : ' + req.body.firstName + ' was successfully added.');
+      // Print the Success on the Success page.
+      res.render('sucess', { title: 'Success', message: 'Successfully added !', name: req.body.firstName, email: req.body.email, users: users });
   }
-    console.log('sucess');
-});
-
-
+});//app.post
 
 app.listen(3000, function () {
   console.log("Server running on port 3000...\nPress Ctrl & C to end the server.");
